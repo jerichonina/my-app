c91a84a663af0fc73cbbe02e0791df8a2d1259ab
Jerichonina <102739349+jerichonina@users.noreply.github.com>
2022-08-20 16:30:09 +0800

    added fontawesome

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
 import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./Weather.css";
 
 export default function Weather(props) {
