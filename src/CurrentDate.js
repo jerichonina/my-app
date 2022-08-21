01e44c64757a2dae12b52cdf2af89bf2dc44dec1
Jerichonina <102739349+jerichonina@users.noreply.github.com>
2022-08-21 14:16:52 +0800

    Update CurrentDate.js

--- src/CurrentDate.js
+++ src/CurrentDate.js
@@ -1,18 +1,7 @@
 import React from "react";
 
 export default function CurrentDate(props) {
-    let days =["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-    let day = days[props.data.getDay()];
-    let hours = props.data.getHours();
-    if (hours <10) {
-        let hours = `0${hours}`;
-    }
-    let minutes = props.data.getMinutes();
-    if (minutes <10) {
-        let minutes = `0${minutes}`;
-    }
- return (
-     <div> {day} {hours}:{minutes} </div>
- );
+    return ("Sunday"
+    );
 }
 
