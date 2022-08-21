efb9731628e61a62599269fa75e76d4f4b7a132a
Jerichonina <102739349+jerichonina@users.noreply.github.com>
2022-08-21 14:29:13 +0800

    edited index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
