d66c8a1a08f3f6ffbfd942c9daca690c75324205
Jerichonina <102739349+jerichonina@users.noreply.github.com>
2022-08-29 22:42:13 +0800

    Update WeatherForecast.js

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -17,7 +17,7 @@ export default function WeatherForecast(props) {
         return (
             <div className = "WeatherForecast">
                 <div className = "row">
-                    {forecast.map(function (dailyForecast, index) {
+                    {forecast.map (function (dailyForecast, index) {
                         if (index < 5) {
                             return (
                     <div className ="col" key ={index}>
